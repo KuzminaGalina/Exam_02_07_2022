0aedc8bb6d5e84f7822db8e681a1ed9c35ed69fc
Anton Stepin <ooosg@rambler.ru>
2022-07-02 17:21:56 +0300

    Добалены исходные данные

--- Exam_02_07_2022/Program.cs
+++ Exam_02_07_2022/Program.cs
@@ -1,3 +1,43 @@
 ﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которым меньше либо равна 3 символам.
 
+string[] NewArray(string[] EnterArray, int simbolLength)
+{
+    int j = 0;
+    for (int i = 0; i < EnterArray.Length; i++)
+    {
+        if (EnterArray[i].Length <= simbolLength)
+        {
+            j++;
+        }
+    }
+    string[] result = new string[j];
+    int k = 0;
+    for (int i = 0; i < EnterArray.Length; i++)
+    {
+        if (EnterArray[i].Length <= simbolLength)
+        {
+            result[k] = EnterArray[i];
+            k++;
+        }
+    }
+    return result;
+}
 
+void printArray(string[] Array)
+{
+   Console.Write("["); 
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Console.Write(Array[i]);
+        if (i < Array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+}
+
+string [] Data = {"2", "нетy", "привет"};
+int M = 3;
+string [] Array = NewArray(Data, M);
+printArray(Array);
